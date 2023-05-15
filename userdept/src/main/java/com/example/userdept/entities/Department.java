08672b75ab36a65d27ccea20640aecd4b8e70c53
Willian Mikhael da Cunha <willian_mikhael@outlook.com>
2023-05-15 16:02:50 -0300

    fisrtCommit

---
+++ userdept/src/main/java/com/example/userdept/entities/Department.java
@@ -0,0 +1,34 @@
+package com.example.userdept.entities;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@Table(name = "tb_department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Department(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
